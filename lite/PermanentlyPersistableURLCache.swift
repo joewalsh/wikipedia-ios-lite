2a48cce4135c9129119305e42b70cccebe3c5493
Natalia Harateh <nambatee@users.noreply.github.com>
2019-02-22 14:38:36 -0500

    create a delegate for persistent cache so it can delegate getting persisted data

--- lite/PermanentlyPersistableURLCache.swift
+++ lite/PermanentlyPersistableURLCache.swift
@@ -1,10 +1,23 @@
 import UIKit
 
+protocol PermanentlyPersistableURLCacheDelegate: AnyObject {
+    func permanentlyPersistedData(for url: URL) -> Data?
+}
+
 class PermanentlyPersistableURLCache: URLCache {
+    weak var delegate: PermanentlyPersistableURLCacheDelegate?
+
     override func cachedResponse(for request: URLRequest) -> CachedURLResponse? {
         guard let response = super.cachedResponse(for: request) else {
-            // return permanenently cashed response
-            return nil
+            guard let url = request.url else {
+                return nil
+            }
+            guard let data = delegate?.permanentlyPersistedData(for: url) else {
+                return nil
+            }
+            // don't set mimeType here, do it in cache controller
+            let response = URLResponse(url: url, mimeType: "text/html", expectedContentLength: data.count, textEncodingName: nil)
+            return CachedURLResponse(response: response, data: data)
         }
         return response
     }
