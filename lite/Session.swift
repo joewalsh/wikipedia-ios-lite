85b0ffe8c0946600e9bda61bd6bdca4e3a2e0084
Natalia Harateh <nambatee@users.noreply.github.com>
2019-02-22 11:33:23 -0500

    remove SessionDelegate

--- lite/Session.swift
+++ lite/Session.swift
@@ -9,7 +9,6 @@ struct Callback {
 
 public class Session: NSObject {
     let sessionConfiguration: URLSessionConfiguration
-    let sessionDelegate: SessionDelegate
     let session: URLSession
     
     enum Result<T> {
@@ -19,74 +18,7 @@ public class Session: NSObject {
     
     override init() {
         sessionConfiguration = URLSessionConfiguration.default
-        sessionDelegate = SessionDelegate()
-        session = URLSession(configuration: sessionConfiguration, delegate: sessionDelegate, delegateQueue: sessionDelegate.delegateQueue)
-    }
-    
-    @discardableResult func executeDataTaskWith(_ request: URLRequest, callback: Callback) -> URLSessionTask {
-        let task = session.dataTask(with: request)
-        sessionDelegate.addCallback(callback: callback, for: task)
-        task.resume()
-        return task
-    }
-}
-
-class SessionDelegate: NSObject, URLSessionDelegate, URLSessionDataDelegate {
-    let delegateDispatchQueue = DispatchQueue(label: "SessionDelegateDispatchQueue", qos: .default, attributes: [.concurrent], autoreleaseFrequency: .workItem, target: nil)
-    let delegateQueue: OperationQueue
-    var callbacks: [Int: Callback] = [:]
-    
-    override init() {
-        delegateQueue = OperationQueue()
-        delegateQueue.underlyingQueue = delegateDispatchQueue
-    }
-    
-    func addCallback(callback: Callback, for task: URLSessionTask) {
-        delegateDispatchQueue.async(flags: .barrier) {
-            self.callbacks[task.taskIdentifier] = callback
-        }
-    }
-    
-    func removeDataCallback(for task: URLSessionTask) {
-        delegateDispatchQueue.async(flags: .barrier) {
-            self.callbacks.removeValue(forKey: task.taskIdentifier)
-        }
-    }
-    
-    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive response: URLResponse, completionHandler: @escaping (URLSession.ResponseDisposition) -> Void) {
-        defer {
-            completionHandler(.allow)
-        }
-        guard let callback = callbacks[dataTask.taskIdentifier]?.response else {
-            return
-        }
-        callback(response)
-    }
-    
-    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
-        guard let callback = callbacks[dataTask.taskIdentifier]?.data else {
-            return
-        }
-        callback(data)
-    }
-    
-    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
-        defer {
-            removeDataCallback(for: task)
-        }
-        
-        guard let callback = callbacks[task.taskIdentifier] else {
-            return
-        }
-        
-        if let error = error as NSError? {
-            if error.domain != NSURLErrorDomain || error.code != NSURLErrorCancelled {
-                callback.failure(error)
-            }
-            return
-        }
-        
-        callback.success()
+        session = URLSession(configuration: sessionConfiguration)
     }
 }
 
