96fe36d7706e14d41746b57fe7b4b217ef85ec7c
Natalia Harateh <nambatee@users.noreply.github.com>
2019-02-21 23:36:41 -0500

    add a method to fetch article html to ArticleFetcher

--- lite/ArticleFetcher.swift
+++ lite/ArticleFetcher.swift
@@ -1,3 +1,9 @@
+import Foundation
+
 class ArticleFetcher: Fetcher {
-    
+
+    func fetchHTML(for articleURL: URL, callback: Callback) {
+        let request = URLRequest(url: articleURL)
+        session.executeDataTaskWith(request, callback: callback)
+    }
 }
