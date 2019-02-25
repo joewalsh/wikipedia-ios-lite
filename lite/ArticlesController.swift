5ef35180b8af8137c50ed2784f6441bb30ea6004
Natalia Harateh <nambatee@users.noreply.github.com>
2019-02-25 16:56:10 -0500

    call fetcher to get media

--- lite/ArticlesController.swift
+++ lite/ArticlesController.swift
@@ -22,24 +22,9 @@ final class ArticlesController: NSObject {
             downloadArticleResourceAndCache(.references, for: articleURL)
             downloadArticleResourceAndCache(.sections, for: articleURL)
             // media response needs to be decoded to cache individual files
-            fetcher.getMedia(for: articleURL) { data, response, error in
-                if let error = error {
-                    fatalError(error.localizedDescription)
-                }
-                guard let response = response as? HTTPURLResponse else {
-                    assertionFailure("Expected HTTP response")
-                    return
-                }
-                guard response.statusCode == 200 else {
-                    assertionFailure("Expected 200 status code, got \(response.statusCode)")
-                    return
-                }
-                guard let data = data else {
-                    assertionFailure("Expected data, got nil")
-                    return
-                }
-                let decoder = JSONDecoder()
-                let media = try? decoder.decode(Media.self, from: data)
+            // should be separated?
+            fetcher.getMedia(for: articleURL) { error, media in
+                assert(error == nil)
                 print(media)
             }
         }
@@ -56,65 +41,6 @@ final class ArticlesController: NSObject {
             self.cacheController.moveTemporaryFileToCache(temporaryFileURL: temporaryFileURL, withContentsOf: resourceURL) { error, key in
                 self.cacheController.addCacheItemToCacheGroup(for: articleURL, cacheItemKey: key)
             }
-        }
-    }
-}
-
-private struct Media: Decodable {
-    let revision: String?
-    let tid: String?
-    let items: [Item]?
-
-    struct Item: Decodable {
-        let sectionID: UInt?
-        let type: String?
-        let caption: Info?
-        let showInGallery: Bool?
-        let titles: Titles?
-        let thumbnail: Image?
-        let original: Image?
-        let filePage: String?
-        let artist: Info?
-        let credit: Info?
-        let license: License?
-        let description: Info?
-
-        enum CodingKeys: String, CodingKey {
-            case sectionID = "section_id"
-            case type
-            case caption
-            case showInGallery
-            case titles
-            case thumbnail
-            case original
-            case filePage = "file_page"
-            case artist
-            case credit
-            case license
-            case description
-        }
-
-        struct Info: Decodable {
-            let html: String?
-            let text: String?
-        }
-
-        struct Titles: Decodable {
-            let canonical: String?
-            let normalized: String?
-            let display: String?
-        }
-
-        struct Image: Decodable {
-            let source: String?
-            let width: UInt?
-            let height: UInt?
-            let mime: String?
-        }
-
-        struct License: Decodable {
-            let type: String?
-            let code: String?
         }
     }
 }
