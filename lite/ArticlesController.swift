d63e420e6ad44f312c7adffbc7bdfea64b0f8b18
Natalia Harateh <nambatee@users.noreply.github.com>
2019-02-28 10:55:29 -0500

    use new methods on ArticlesCacheController

--- lite/ArticlesController.swift
+++ lite/ArticlesController.swift
@@ -18,49 +18,10 @@ final class ArticlesController: NSObject {
             cacheController.removeCachedArticle(with: articleURL)
         } else {
             print("ArticlesController: cache for \(articleURL) doesn't exist, fetching")
-            downloadArticleResourceAndCache(.mobileHTML, for: articleURL)
-            downloadArticleResourceAndCache(.references, for: articleURL)
-            downloadArticleResourceAndCache(.sections, for: articleURL)
-            // media response needs to be decoded to cache individual files
-            // should be separated?
-            fetcher.getMedia(for: articleURL) { error, media in
-                if let error = error {
-                    assertionFailure(error.localizedDescription)
-                }
-                guard let media = media else {
-                    return
-                }
-                self.cacheController.cacheMedia(media, for: articleURL) { imageURL, imageKey in
-                    if let imageURL = imageURL, let imageKey = imageKey {
-                        self.fetcher.downloadImage(imageURL) { error, fileURL in
-                            if let error = error {
-                                assertionFailure(error.localizedDescription)
-                            }
-                            guard let fileURL = fileURL else {
-                                fatalError()
-                            }
-                            self.cacheController.moveTemporaryFileToCache(temporaryFileURL: fileURL, key: imageKey)
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    private func downloadArticleResourceAndCache(_ resource: Configuration.MobileAppsServices.Page.Resource, for articleURL: URL) {
-        fetcher.downloadArticleResource(resource, for: articleURL) { error, temporaryFileURL, resourceURL in
-            if let error = error {
-                fatalError(error.localizedDescription)
-            }
-            guard let temporaryFileURL = temporaryFileURL, let resourceURL = resourceURL else {
-                fatalError()
-            }
-            self.cacheController.moveTemporaryFileToCache(temporaryFileURL: temporaryFileURL, withContentsOf: resourceURL) { error, key in
-                guard let key = key else {
-                    fatalError()
-                }
-                self.cacheController.addCacheItemToCacheGroup(for: articleURL, cacheItemKey: key)
-            }
+            cacheController.cacheResource(.mobileHTML, for: articleURL)
+            cacheController.cacheResource(.references, for: articleURL)
+            cacheController.cacheResource(.sections, for: articleURL)
+            cacheController.cacheMedia(for: articleURL)
         }
     }
 }
