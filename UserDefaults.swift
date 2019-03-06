c292a3f5984d948203a5e11d6d3c5c374e2d3793
Natalia Harateh <nambatee@users.noreply.github.com>
2019-03-06 10:25:18 -0500

    set up user defaults for theme

--- UserDefaults.swift
+++ UserDefaults.swift
@@ -3,6 +3,27 @@ import Foundation
 extension UserDefaults {
     enum Key: String {
         case collapseTables
+        case theme
+    }
+
+    enum Theme: Int {
+        case `default`
+        case sepia
+        case dark
+        case black
+
+        var name: String {
+            switch self {
+            case .default:
+                return "DEFAULT"
+            case .sepia:
+                return "SEPIA"
+            case .dark:
+                return "DARK"
+            case .black:
+                return "BLACK"
+            }
+        }
     }
 
     @objc dynamic var collapseTables: Bool {
@@ -13,4 +34,29 @@ extension UserDefaults {
             set(newValue, forKey: Key.collapseTables.rawValue)
         }
     }
+
+    var theme: Theme? {
+        get {
+            guard
+                let theme = Theme(rawValue: integer(forKey: Key.theme.rawValue))
+            else {
+                return nil
+            }
+            return theme
+        }
+        set {
+            set(newValue?.rawValue, forKey: Key.theme.rawValue)
+            notify(with: UserDefaults.didChangeThemeNotification, object: theme)
+        }
+    }
+
+    private func notify(with notificationName: Notification.Name, object: Any?) {
+        DispatchQueue.main.async {
+            NotificationCenter.default.post(name: notificationName, object: object)
+        }
+    }
+}
+
+extension UserDefaults {
+    static let didChangeThemeNotification = Notification.Name(rawValue: "didChangeThemeNotification")
 }
