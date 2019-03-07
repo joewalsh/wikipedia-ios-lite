c1dc8c21b7e1aa14701a567635aaf77f8b3279f6
Natalia Harateh <nambatee@users.noreply.github.com>
2019-03-07 11:00:13 -0500

    set up theme pref view

--- lite/ThemePreference.swift
+++ lite/ThemePreference.swift
@@ -1,13 +1,31 @@
-//  
-//  ThemePreference.swift
-//  lite
-//
-//  Created by Natalia Harateh on 3/6/19.
-//  Copyright © 2019 Wikimedia Foundation. All rights reserved.
-//
-
 import UIKit
 
 class ThemePreference: UIView, Nibbed {
     @IBOutlet private var themeButtons: [UIButton] = []
+    private var selectedButton: UIButton?
+    let defaults = UserDefaults.standard
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        assert(Theme.Kind.allCases.count == themeButtons.count)
+        for (kind, button) in zip(Theme.Kind.allCases, themeButtons) {
+            button.tag = kind.rawValue
+            let isCurrentTheme = kind.rawValue == defaults.theme.rawValue
+            if isCurrentTheme {
+                button.isSelected = true
+                selectedButton = button
+            }
+            button.setTitle(kind.name, for: .normal)
+        }
+    }
+
+    @IBAction private func updateThemePreference(_ sender: UIButton) {
+        guard let theme = Theme.Kind(rawValue: sender.tag) else {
+            return
+        }
+        selectedButton?.isSelected = false
+        sender.isSelected = true
+        selectedButton = sender
+        defaults.theme = theme
+    }
 }
