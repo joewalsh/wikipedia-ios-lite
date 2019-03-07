5d0089c00dd6f705f67d6e358472794e7c685447
Natalia Harateh <nambatee@users.noreply.github.com>
2019-03-07 14:37:06 -0500

    expose theme script source so that web view can use it

--- lite/WKUserScripts/ThemeUserScript.swift
+++ lite/WKUserScripts/ThemeUserScript.swift
@@ -1,13 +1,34 @@
 import WebKit
 
 final class ThemeUserScript: UserScriptWithCompletion<() -> Void> {
+    static func source(with theme: Theme, messageHandlerName: String? = nil) -> String {
+        let callback: String?
+        if let messageHandlerName = messageHandlerName {
+            callback = """
+            window.webkit.messageHandlers.\(messageHandlerName).postMessage({});
+            """
+        } else {
+            callback = nil
+        }
+
+        let source: String
+        if let callback = callback {
+            source = """
+            window.wmf.setTheme('\(theme.kind.jsName)', () => {
+                \(callback)
+            })
+            """
+        } else {
+            source = """
+            window.wmf.setTheme('\(theme.kind.jsName)')
+            """
+        }
+        return source
+    }
+
     init(theme: Theme, completion: Completion? = nil) {
         let messageHandlerName = "wmfThemeApplied"
-        let source = """
-        window.wmf.setTheme('\(theme.kind.jsName)', () => {
-            window.webkit.messageHandlers.\(messageHandlerName).postMessage({});
-        })
-        """
+        let source = ThemeUserScript.source(with: theme, messageHandlerName: messageHandlerName)
         super.init(source: source, injectionTime: .atDocumentEnd, forMainFrameOnly: true, messageHandlerName: messageHandlerName, completion: completion)
     }
 
