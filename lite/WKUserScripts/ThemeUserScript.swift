5e6750aa426df7a279f55eb112741b2cc7d8a783
Natalia Harateh <nambatee@users.noreply.github.com>
2019-03-06 20:05:34 -0500

    update handler name, handle completion

--- lite/WKUserScripts/ThemeUserScript.swift
+++ lite/WKUserScripts/ThemeUserScript.swift
@@ -2,12 +2,16 @@ import WebKit
 
 final class ThemeUserScript: UserScriptWithCompletion<() -> Void> {
     init(theme: UserDefaults.Theme, completion: Completion? = nil) {
-        let messageHandlerName = "wmf.theme.applied"
+        let messageHandlerName = "wmfThemeApplied"
         let source = """
-        window.wmf.setTheme(\(theme.name), () => {
-            window.webkit.messageHandlers.\(messageHandlerName).postMessage();
-        });
+        window.wmf.setTheme('\(theme.name)', () => {
+            window.webkit.messageHandlers.\(messageHandlerName).postMessage({});
+        })
         """
         super.init(source: source, injectionTime: .atDocumentEnd, forMainFrameOnly: true, messageHandlerName: messageHandlerName, completion: completion)
+    }
+
+    override func handleCompletion(receivedMessage message: WKScriptMessage) {
+        completion?()
     }
 }
