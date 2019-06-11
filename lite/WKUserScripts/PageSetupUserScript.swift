8a81fddeec0f516df36c043a68460935e8ca12da
Natalia Harateh <nambatee@users.noreply.github.com>
2019-06-11 11:43:05 -0400

    remove ThemeUserScript and TablesUserScript add PageSetupUserScript

---
+++ lite/WKUserScripts/PageSetupUserScript.swift
@@ -0,0 +1,27 @@
+import WebKit
+
+final class PageSetupUserScript: UserScriptWithCompletion<() -> Void> {
+    init(theme: Theme, dimImages: Bool, collapseTables: Bool, completion: Completion? = nil) {
+        let messageHandlerName = "wmfPageSetup"
+
+        let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
+        let source = """
+        pagelib.c1.PageMods.setMulti(document, {
+        platform: pagelib.c1.Platforms.IOS,
+        clientVersion: '\(version)',
+        theme: pagelib.c1.Themes.\(theme.kind.jsName),
+        dimImages: \(dimImages),
+        margins: { top: '32px', right: '32px', bottom: '32px', left: '32px' },
+        areTablesCollapsed: \(collapseTables),
+        scrollTop: 64
+        }, () => {
+        window.webkit.messageHandlers.\(messageHandlerName).postMessage({})
+        })
+        """
+        super.init(source: source, injectionTime: .atDocumentEnd, forMainFrameOnly: true, messageHandlerName: messageHandlerName, completion: completion)
+    }
+
+    override func handleCompletion(receivedMessage message: WKScriptMessage) {
+        completion?()
+    }
+}
